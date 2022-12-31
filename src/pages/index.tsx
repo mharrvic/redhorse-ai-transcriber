3831a096b7cee64f5ea82921a71e5b8d6a06c011
mac <mharrvic@gmail.com>
2022-12-31 15:10:30 +0800

    Use audio recorder component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-
-import { trpc } from "../utils/trpc";
+import AudioRecorder from "../component/AudioRecorder";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const { data: sessionData } = useSession();
 
   return (
     <>
@@ -15,18 +14,34 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <main className="min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <nav className="flex">
+          <p className="px-10 py-3 text-xl text-white">
+            Redhorse AI Transcriber
+          </p>
+          <div className="ml-auto">
+            <button
+              className="bg-gray/10 hover:bg-gray/20 rounded-full px-10 py-3 font-semibold text-white no-underline transition"
+              onClick={sessionData ? () => signOut() : () => signIn("google")}
+            >
+              {sessionData ? "Sign out" : "Sign in"}
+            </button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Hello!
+            {sessionData ? (
+              <>
+                <span className="text-[#ff6b00]">
+                  {sessionData.user?.email}
+                </span>
+                <AudioRecorder />
+              </>
+            ) : (
+              <span className="text-[#ff6b00]"> Guest</span>
+            )}
           </h1>
-
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
-          </div>
         </div>
       </main>
     </>
@@ -34,27 +49,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn("google")}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
