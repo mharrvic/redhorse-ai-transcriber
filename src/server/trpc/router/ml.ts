7dde5bb9c837853b3280f2cf4893acb9e95779d9
mac <mharrvic@gmail.com>
2023-01-01 18:08:45 +0800

    Set api route to public

--- src/server/trpc/router/ml.ts
+++ src/server/trpc/router/ml.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 import { transcribeAudio } from "../../../utils/banana";
 
-import { protectedProcedure, router } from "../trpc";
+import { publicProcedure, router } from "../trpc";
 
 export const mlRouter = router({
-  transcribe: protectedProcedure
+  transcribe: publicProcedure
     .input(z.object({ mp3BytesString: z.string() }))
     .mutation(async ({ input }) => {
       const transcribe = await transcribeAudio(input.mp3BytesString);
