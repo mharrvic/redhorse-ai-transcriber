5034eeb05e30129dfa6e5c46c20db58bc2eac3e2
mac <mharrvic@gmail.com>
2023-01-01 17:47:39 +0800

    Fix template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { type NextPage } from "next";
-import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
 import { Toaster } from "react-hot-toast";
 import AudioRecorder from "../component/AudioRecorder";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
-
   return (
     <>
       <Head>
@@ -18,18 +15,33 @@ const Home: NextPage = () => {
 
       <main className="min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <nav className="flex">
-          <p className="px-10 py-3 text-xl text-white flex">
-            <Image src="/redhorse-ai-transcriber.png" alt="Logo" width="100" height="100"/>
+          <p className="flex px-10 py-3 text-xl text-white">
+            <Image
+              src="/redhorse-ai-transcriber.png"
+              alt="Redhorse AI Transcriber Logo"
+              width="100"
+              height="100"
+            />
           </p>
         </nav>
         <div className="mx-4 px-4 py-16 text-center">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-              <>
-                <p className="text-white"> Redhorse AI <span className="text-[#ff6b00]">Transcriber</span></p>   
-                <p className="font-mono text-lg text-gray-400 mb-4">Openai's Whisper (Automatic Speech Recognition) deployed in a Serverless GPU (banana.dev)</p>         
-                <p className="font-mono text-sm text-gray-200">1. Click Record (to record) -> 2. Stop (to stop the recording) -> and 3. Transcribe (to start the transcribe process)</p>
-                <AudioRecorder />
-              </>
+            <>
+              <p className="text-white">
+                {" "}
+                Redhorse AI <span className="text-[#ff6b00]">Transcriber</span>
+              </p>
+              <p className="mb-4 font-mono text-lg text-gray-400">
+                Openai&apos;s Whisper (Automatic Speech Recognition) deployed in
+                a Serverless GPU (banana.dev)
+              </p>
+              <p className="font-mono text-sm text-gray-200">
+                1. Click Record (to record) -&gt; 2. Stop (to stop the
+                recording) -&gt; and 3. Transcribe (to start the transcribe
+                process)
+              </p>
+              <AudioRecorder />
+            </>
           </h1>
         </div>
       </main>
